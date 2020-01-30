d4c5b316cb5a9114161af8b070f98cf476129e82
Gabriel <gabrielperren@gmail.com>
2020-01-30 09:56:31 -0300

    PEP8!

--- CI.py
+++ CI.py
@@ -1,13 +1,15 @@
 import numpy as np
 from scipy.spatial import distance
-from scipy import spatial
+# from scipy import spatial
 from astropy.table import Table
+
 
 def read_data(file_name):
     data = Table.read(file_name, format='ascii')
     data = data['ID', 'x', 'y', 'V', 'BV', 'pmRA', 'pmDE']
     return data['ID'], data['x'], data['y'], data['V'],\
         data['BV'], data['pmRA'], data['pmDE']
+
 
 def main(rad, cent, file_name):
     # Read data
@@ -24,9 +26,11 @@ def main(rad, cent, file_name):
             dim_a_member.append(pmRA[i])
         else:
             dim_b_member.append(pmRA[i])
-    
-    # For each star in A, find the 10 nearest neighbors in B and select the one with the greatest distance
-    # For each star in A, find the 10 nearest neighbors in A and select de one with the greatest distance
+
+    # For each star in A, find the 10 nearest neighbors in B and select the
+    # one with the greatest distance
+    # For each star in A, find the 10 nearest neighbors in A and select thee
+    # one with the greatest distance
     dist_a_b, dist_a_a = [], []
     rad_ab, rad_aa = [], []
     for i in range(len(dim_a_member)):
@@ -36,22 +40,21 @@ def main(rad, cent, file_name):
         rad_ab.append(sorted(dist_a_b[i])[9])
         rad_aa.append(sorted(dist_a_a[i])[10])
 
-    # Calculate the average neighbor density associated with each star of A in B, and idem for A in A
+    # Calculate the average neighbor density associated with each star of A
+    # in B, and idem for A in A
     d_ab, d_aa = [], []
     for i in range(len(dim_a_member)):
-        d_ab.append(10/(np.pi*rad_ab[i]**2))
-        d_aa.append(10/(np.pi*rad_aa[i]**2))
+        d_ab.append(10 / (np.pi * rad_ab[i]**2))
+        d_aa.append(10 / (np.pi * rad_aa[i]**2))
     d_ab = np.mean(d_ab)
     d_aa = np.mean(d_aa)
 
-    CI = d_ab/d_aa
+    CI = d_ab / d_aa
+    print(CI)
 
-
-
-  
-    
+    # Unused?
     A_parameter, B_paramater = [], []
-   
+
 
 if __name__ == '__main__':
     main(250., [(1000., 1000.)], 'input/synth_clust_out_0.6.dat')

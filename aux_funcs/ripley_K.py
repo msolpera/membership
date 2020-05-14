f13cbb2fe6ea127fa615cd49873a29981655942d
Gabriel <gabrielperren@gmail.com>
2020-05-14 16:50:51 -0300

    function to generate the table of RK values

---
+++ aux_funcs/ripley_K.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from astropy.stats import RipleysKEstimator
+from astropy.io import ascii
+from astropy.table import Table
+from astropy.table import Column
+
+N_max, N_runs = 500, 100
+
+Kest = RipleysKEstimator(area=1)  # , x_max=1, y_max=1, x_min=0, y_min=0)
+N_vals = np.arange(5, N_max)
+
+data_dict = {}
+for RK_rad in (.5,): #(.1, .2, .3, .4, .5, .6, .7, .8, .9):
+    print(RK_rad)
+    data_dict[str(RK_rad) + '_m'] = []
+    data_dict[str(RK_rad) + '_s'] = []
+    for N in N_vals:
+        if not (N % 50):
+            print(N)
+        C_vals = []
+        for _ in range(N_runs):
+            xy_u = np.random.uniform(0., 1., (N, 2))
+            C_vals.append(Kest(xy_u, (RK_rad,)))
+
+        data_dict[str(RK_rad) + '_m'] += [np.round(np.mean(C_vals), 4)]
+        data_dict[str(RK_rad) + '_s'] += [np.round(np.std(C_vals), 4)]
+
+# Store in data file
+tt = Table(data_dict)
+col = Column(N_vals, name='N')
+tt.add_column(col, index=0)
+ascii.write(tt, "RK_data.dat", format='csv', overwrite=True)

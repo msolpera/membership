2069abe4cf857e4c06acb4d8755812e89e857739
Gabriel <gabrielperren@gmail.com>
2020-06-01 15:17:52 -0300

    deleted files (most where moved elsewhere)

--- modules/UP.py
+++
@@ -1,7 +0,0 @@
-
-from astropy.table import Table
-
-
-def main(file_name):
-    data = Table.read(file_name, format='ascii')
-    return data['ID'], data['probability']
--- modules/random_memb.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-
-
-def main(ID):
-    memb_prob = np.random.uniform(0., 1., len(ID))
-    return memb_prob

b03b92f1693a539e0a4774ebdc2df6f48e974914
Gabriel <gabrielperren@gmail.com>
2020-06-01 18:05:15 -0300

    more test plots

--- aux_funcs/UPMASK/plot_RESULTS.py
+++ aux_funcs/UPMASK/plot_RESULTS.py
@@ -93,67 +93,88 @@ def resultsProcess(data, Nruns=50, ID_col=False):
     plt.legend()
 
 
-# plt.subplot(131)
-# resultsProcess(ascii.read("output/up-RESULTS_R_50_1.dat"))
-# plt.title("R 1, 50")
+plt.subplot(231)
+resultsProcess(ascii.read("output/up-RESULTS_R_50_1.dat"))
+plt.title("R 1, 50")
 
-# plt.subplot(132)
-# resultsProcess(ascii.read("output/up-RESULTS_R_50_2.dat"))
-# plt.title("R 2, 50")
+plt.subplot(232)
+resultsProcess(ascii.read("output/up-RESULTS_R_50_2.dat"))
+plt.title("R 2, 50")
 
-# # plt.subplot(233)
-# # resultsProcess(ascii.read("output/up-RESULTS_Subl_50_1.dat"))
-# # plt.title("Subl 1, 50")
+# plt.subplot(233)
+# resultsProcess(ascii.read("output/up-RESULTS_Subl_50_1.dat"))
+# plt.title("Subl 1, 50")
 
-# # plt.subplot(234)
-# # resultsProcess(ascii.read("output/up-RESULTS_Subl_50_2.dat"))
-# # plt.title("Subl 2, 50")
+# plt.subplot(234)
+# resultsProcess(ascii.read("output/up-RESULTS_Subl_50_2.dat"))
+# plt.title("Subl 2, 50")
 
-# plt.subplot(133)
+plt.subplot(233)
+name = "output/pca_check/up-RESULTS_010_pca2_1.dat"
+print(name)
+resultsProcess(ascii.read(name))
+plt.title("N_PCA=2, 1")
+
+plt.subplot(234)
+name = "output/pca_check/up-RESULTS_010_pca2_2.dat"
+print(name)
+resultsProcess(ascii.read(name))
+plt.title("N_PCA=2, 2")
+
+plt.subplot(235)
+name = "output/pca_check/up-RESULTS_010_pca2_3.dat"
+print(name)
+resultsProcess(ascii.read(name))
+plt.title("N_PCA=2, 3")
+
+# plt.subplot(246)
 # resultsProcess(ascii.read("output/0.1up_50_50.dat"))
-# plt.title("R Sol 1, 50")
+# plt.title("R Sol (N_PCA=4), 50")
 
+plt.subplot(236)
+resultsProcess(ascii.read("output/up-RESULTS_xycut.dat"))
+plt.title("R xycut, 50")
 
 
 # plt.subplot(241)
 # resultsProcess(
-#     ascii.read("output/up-0.80_0.49_0.49_0.33_0.29_R_50_1.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/up-0.80_0.49_0.49_0.33_0.29_R_50_1.dat"), ID_col=True)
 # plt.title("R 1, 50")
 
 # plt.subplot(242)
 # resultsProcess(
-#     ascii.read("output/up-0.80_0.49_0.49_0.33_0.29_Subl_50_1.dat"),
+#     ascii.read("output/K_KDE_test/up-0.80_0.49_0.49_0.33_0.29_Subl_50_1.dat"),
 #     ID_col=True)
 # plt.title("R 2, 50")
 
 # plt.subplot(243)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_R_R.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_R_R.dat"), ID_col=True)
 # plt.title("Python R R, 50")
 
 # plt.subplot(244)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_R_PR.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_R_PR.dat"), ID_col=True)
 # plt.title("Python R PR, 50")
 
 # plt.subplot(245)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_R_P.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_R_P.dat"), ID_col=True)
 # plt.title("Python R P, 50")
 
 # plt.subplot(246)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_P_R.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_P_R.dat"), ID_col=True)
 # plt.title("Python P R, 50")
 
 # plt.subplot(247)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_P_PR.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_P_PR.dat"), ID_col=True)
 # plt.title("Python P PR, 50")
 
 # plt.subplot(248)
 # resultsProcess(
-#     ascii.read("output/0.80_0.49_0.49_0.33_0.29_P_P.dat"), ID_col=True)
+#     ascii.read("output/K_KDE_test/0.80_0.49_0.49_0.33_0.29_P_P.dat"), ID_col=True)
 # plt.title("Python P P, 50")
 
 
@@ -194,40 +215,40 @@ def resultsProcess(data, Nruns=50, ID_col=False):
 # plt.title("kdetest, GUMM")
 
 
-plt.subplot(231)
-name = "output/pca_check/up-RESULTS_010_pca2_1.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=2, 1")
+# plt.subplot(231)
+# name = "output/pca_check/up-RESULTS_010_pca2_1.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=2, 1")
 
-plt.subplot(232)
-name = "output/pca_check/up-RESULTS_010_pca2_2.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=2, 2")
+# plt.subplot(232)
+# name = "output/pca_check/up-RESULTS_010_pca2_2.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=2, 2")
 
-plt.subplot(233)
-name = "output/pca_check/up-RESULTS_010_pca2_3.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=2, 3")
+# plt.subplot(233)
+# name = "output/pca_check/up-RESULTS_010_pca2_3.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=2, 3")
 
-plt.subplot(234)
-name = "output/pca_check/0.1up_50_50_ndims4_1.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=4, 1")
+# plt.subplot(234)
+# name = "output/pca_check/0.1up_50_50_ndims4_1.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=4, 1")
 
-plt.subplot(235)
-name = "output/pca_check/0.1up_50_50_ndims4_2.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=4, 2")
+# plt.subplot(235)
+# name = "output/pca_check/0.1up_50_50_ndims4_2.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=4, 2")
 
-plt.subplot(236)
-name = "output/pca_check/0.1up_50_50_ndims4_3.dat"
-print(name)
-resultsProcess(ascii.read(name))
-plt.title("N_PCA=4, 3")
+# plt.subplot(236)
+# name = "output/pca_check/0.1up_50_50_ndims4_3.dat"
+# print(name)
+# resultsProcess(ascii.read(name))
+# plt.title("N_PCA=4, 3")
 
 plt.show()

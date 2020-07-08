f38aa6e65709e7573b60fcf55946b01e8bf64164
Gabriel <gabrielperren@gmail.com>
2020-07-08 11:00:06 -0300

    minor, testing Ripley K

--- aux_funcs/ripley_K.py
+++ aux_funcs/ripley_K.py
@@ -10,26 +10,28 @@ Generate a table with the results of applying Ripley's K function over
 2D uniform random fields created with different number of elements.
 """
 
-N_max, N_runs = 2000, 1000
+Kmode = 'translation'
+N_max, N_runs = 500, 50
 
-Kest = RipleysKEstimator(area=1)  # , x_max=1, y_max=1, x_min=0, y_min=0)
+Kest = RipleysKEstimator(area=1, x_max=1, y_max=1, x_min=0, y_min=0)
 N_vals = np.arange(5, N_max)
 
 data_dict = {}
-for RK_rad in (.1, .2, .3, .4, .5, .6, .7, .8, .9):
+for RK_rad in (.2, .3, .4, .5):
     print(RK_rad)
-    data_dict[str(RK_rad) + '_m'] = []
-    data_dict[str(RK_rad) + '_s'] = []
+    data_dict['mean_{:.1f}'.format(RK_rad)] = []
+    data_dict['std_{:.1f}'.format(RK_rad)] = []
     for N in N_vals:
         if not (N % 50):
             print(N)
         C_vals = []
         for _ in range(N_runs):
             xy_u = np.random.uniform(0., 1., (N, 2))
-            C_vals.append(Kest(xy_u, (RK_rad,)))
+            C_vals.append(Kest(xy_u, (RK_rad,), mode=Kmode))
 
-        data_dict[str(RK_rad) + '_m'] += [np.round(np.mean(C_vals), 4)]
-        data_dict[str(RK_rad) + '_s'] += [np.round(np.std(C_vals), 4)]
+        data_dict['mean_{:.1f}'.format(RK_rad)] += [
+            np.round(np.mean(C_vals), 4)]
+        data_dict['std_{:.1f}'.format(RK_rad)] += [np.round(np.std(C_vals), 4)]
 
 # Store in data file
 tt = Table(data_dict)
--- aux_funcs/ripley_k_test.py
+++ aux_funcs/ripley_k_test.py
@@ -2,132 +2,160 @@
 import numpy as np
 from astropy.stats import RipleysKEstimator
 import matplotlib.pyplot as plt
+import time as t
 
 
-def stdRadTest():
+def RKfunc(Kest, N, rad, mode, N_C_ran=100):
+    # https://stats.stackexchange.com/a/122816/10416
+    C_vals = []
+    for _ in range(N_C_ran):
+        xy_u = np.random.uniform(0., 1., (N, 2))
+        C_vals.append(Kest(xy_u, (rad,), mode=mode))
+
+    return np.mean(C_vals), np.std(C_vals)
+
+
+def MCCmetric(TP, FP, TN, FN):
     """
-    Test Ripley's K function discrimination power using the mean and standard
-    deviation of a set of random uniform fields, for several values of the
-    'C_s' parameter (distance in standard deviations)
     """
+    sqrt = np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+    sqrt = 1. if sqrt == 0. else sqrt
+    MCC = ((TP * TN) - (FP * FN)) / sqrt
 
-    def RKfunc(Kest, N, rad, N_C_ran=100):
-        # https://stats.stackexchange.com/a/122816/10416
-        C_vals = []
-        for _ in range(N_C_ran):
-            xy_u = np.random.uniform(0., 1., (N, 2))
-            C_vals.append(Kest(xy_u, (rad,)))
-
-        return np.mean(C_vals), np.std(C_vals)
-
-    # Cluster number of stars (fixed for all runs)
-    N = 20
-    std_list = (1., 2., 3.)
-    rad_lst = (0.5,)
-    # Define Ripley's K
-    Kest = RipleysKEstimator(area=1, x_max=1, y_max=1, x_min=0, y_min=0)
-
-    results = []
-    for C_thresh in std_list:
-        print(C_thresh)
-        for rad in rad_lst:
-            print(rad)
-            vals_C_r = []
-            for _ in range(100):
-                # Standard deviation of the defined cluster
-                xy_std = np.random.uniform(0.01, 0.5)
-                xy = np.random.normal((.5, .5), xy_std, (N, 2))
-
-                # Process random uniform distributions
-                mean_k, std_k = RKfunc(Kest, N, rad)
-
-                # When the observed K value is larger than the expected K
-                # value for a particular distance, the distribution is more
-                # clustered than a random distribution.
-                C_s = (Kest(xy, (rad,)) - mean_k) / std_k
-
-                # We consider 0.25 the limit between cluster and field
-                # This is a cluster
-                if xy_std <= 0.25:
-                    if C_s < C_thresh:
-                        # Test failed
-                        vals_C_r.append(0.)
-                    else:
-                        # Test was successful
-                        vals_C_r.append(1.)
-                # THis is a random field
-                else:
-                    if C_s < C_thresh:
-                        vals_C_r.append(1.)
-                    else:
-                        vals_C_r.append(0.)
-
-            results.append([
-                C_thresh, rad, np.mean(vals_C_r), np.std(vals_C_r)])
-
-    results = np.array(results)
-    print(results)
+    return MCC
 
 
 def successTest():
     """
-    Test Ripley's K function to see which radius has the most "separating
-    power" to discriminate between a uniform 2D random distribution, and a
-    2D cluster.
+    Test Ripley's K function to see which configuration has the most
+    "separating power" to discriminate between a uniform 2D random
+    distribution, and a 2D cluster.
     """
 
-    K_method = 'none'  # 'poisson' 'ohser'  'var-width' 'ripley' 'translation'
-    print("Method: ", K_method)
+    # If this mode is 'True', it will compare each method in 'K_methods'
+    # with the value for a Poisson homogeneous process (which depends on the
+    # radius value exclusively).
+    poisson_mode = False
+
+    # This is required since under 'poisson_mode' there is no standard
+    # deviation and hence the only threshold that means anything is zero.
+    if poisson_mode:
+        std_list = (0.,)
+    else:
+        std_list = (1., 2., 3.)
+
+    rad_vals = np.arange(.1, .501, .1)
+    N_vals = (10, 25, 50)
+    # 'var-width',
+    K_methods = ('none', 'ohser', 'ripley', 'translation')
+
     Kest = RipleysKEstimator(area=1, x_max=1, y_max=1, x_min=0, y_min=0)
-    rad_vals = np.linspace(.1, .9, 100)
 
-    for N in (5, 25, 50):
-        print(N)
-        success = []
-        for i, rad in enumerate(rad_vals):
-            if not (i + 1) % 50:
-                print(" r=", rad)
+    labels = ["{:.2f}".format(_) for _ in rad_vals]
+    x = np.arange(len(labels))  # the label locations
+    width = 0.2  # the width of the bars
+    if poisson_mode:
+        widths = (0.,)
+    else:
+        widths = (-width, 0., width)
 
-            if K_method == 'poisson':
-                K_un = Kest.poisson(rad)
+    N_run = 2000 if poisson_mode else 100
 
-            rad_res = []
-            for _ in range(100):
+    for K_method in K_methods:
+        print("Method: ", K_method)
 
-                # Create a cluster
-                xy_std = np.random.uniform(0.05, 0.3)
-                xy = np.random.normal((.5, .5), xy_std, (N, 2))
+        fig, ax123 = plt.subplots(3, 1, figsize=(10, 12))
+        start_t = t.time()
+        for iN, N in enumerate(N_vals):
+            print(" ", N)
 
-                # Test cluster with a given radius
-                K_cl = Kest(xy, (rad,))
+            ax123[iN].set_title("N={}".format(N))
 
-                if K_method != 'poisson':
-                    # Create random uniform distribution (RUD)
-                    xy_u = np.random.uniform(0., 1., (N, 2))
-                    # Test RUD with the same radius
-                    K_un = Kest(xy_u, (rad,))
+            summary = []
+            for C_thresh in std_list:
+                print("  ", C_thresh)
 
-                # If it assigned a larger K to the cluster, it means that it
-                # was able to identify this as a non RUD; in this case store
-                # a value of 1. Otherwise (was not able to correctly identify
-                # the cluster) store a value of 0.
-                if K_cl > K_un:
-                    rad_res.append(1.)
-                else:
-                    rad_res.append(0.)
+                C_thresh_lst = []
+                for rad in rad_vals:
+                    print("   ", rad)
 
-            success.append(rad_res)
+                    # Mean and stddev for a number of RUDs
+                    if poisson_mode:
+                        mean_k, std_k = Kest.poisson(rad), 1.
 
-        # Mean success per radius value.
-        success = np.mean(success, 1)
-        plt.plot(rad_vals, success, label=r"$N=${}".format(N))
+                    TP, FP, TN, FN = 0., 0., 0., 0.
+                    for _ in range(N_run):
 
-    plt.title("Method: {}".format(K_method))
-    plt.legend()
-    plt.xlabel("rad")
-    plt.ylabel("Success rate")
-    plt.show()
+                        if poisson_mode is False:
+                            # Process random uniform distributions
+                            mean_k, std_k = RKfunc(Kest, N, rad, K_method)
+
+                        # Test a real cluster
+                        xy_std = np.random.uniform(0.05, 0.3)
+                        xy = np.random.normal((.5, .5), xy_std, (N, 2))
+                        # When the observed K value is larger than the
+                        # expected K value for a particular distance, the
+                        # distribution is more clustered than a random
+                        # distribution.
+                        C_s = (
+                            Kest(xy, (rad,), mode=K_method) - mean_k) / std_k
+                        # If C_s is larger than the threshold, the method
+                        # correctly identified a real cluster.
+                        if C_s >= C_thresh:
+                            # Test was successful
+                            TP += 1
+                        else:
+                            # Test failed
+                            FN += 1
+
+                        # # Test a RUD
+                        # xy_u = np.random.uniform(0., 1., (N, 2))
+                        # C_s = (
+                        #     Kest(xy_u, (rad,), mode=K_method) - mean_k) / std_k
+                        # if C_s > C_thresh:
+                        #     FP += 1
+                        # else:
+                        #     TN += 1
+
+                    # C_thresh_lst.append(MCCmetric(TP, FP, TN, FN))
+                    C_thresh_lst.append(TP - FN)
+
+                # summary.append(C_thresh_lst)
+                summary.append(np.array(C_thresh_lst) / N_run)
+
+            def autolabel(ax, rects):
+                for rect in rects:
+                    height = rect.get_height()
+                    ax.annotate(
+                        '{:.2f}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3), textcoords="offset points", ha='center',
+                        va='bottom')
+
+            for iC, Cm in enumerate(summary):
+                rect = ax123[iN].bar(
+                    x + widths[iC], Cm, width,
+                    label=r"$C_s=${}".format(std_list[iC]))
+                autolabel(ax123[iN], rect)
+
+            ax123[iN].set_xticks(x)
+            ax123[iN].set_xticklabels(labels)
+            ax123[iN].set_ylim(.0, 1.1)
+            ax123[iN].axhline(1., ls=':', c='k')
+            if iN == 0:
+                ax123[iN].legend()
+            plt.xlabel("rad")
+            # ax123[iN].set_ylabel("MCC")
+            ax123[iN].set_ylabel("TP-FN")
+
+        elapsed = t.time() - start_t
+        plt.suptitle(
+            "Method: {}, ({:.0f} secs)".format(K_method, elapsed), y=1.01)
+        fig.tight_layout()
+        pp = "_poisson" if poisson_mode else ""
+        plt.savefig(
+            "K_test_{}{}.png".format(K_method, pp), dpi=150,
+            bbox_inches='tight')
 
 
-# successTest()
-stdRadTest()
+successTest()

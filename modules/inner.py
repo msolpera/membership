c3080a4a13aaa6a55c9308671ad5fc84acca9288
Gabriel <gabrielperren@gmail.com>
2020-08-03 18:53:56 -0300

    better n_clusters & C_masks handling

--- modules/inner.py
+++ modules/inner.py
@@ -7,23 +7,32 @@ from . import clustAlgor
 
 def main(
     clust_xy, clust_data, N_membs, clust_method, clRjctMethod,
-        KDE_vals, Kest, C_thresh, cl_method_pars, prfl, N_cl_max=1000):
+    KDE_vals, Kest, C_thresh, cl_method_pars, prfl, N_cl_max=10000,
+        N_st_max=20000):
     """
     Perform the inner loop: cluster --> reject
+
+    N_cl_max : maximum number of clusters allowed
+    N_st_max : maximum numbers of stars for the Voronoi and kNNdens algorithms.
+               Above this threshold, the 'cdist' function is avoided.
     """
+
     print("  Performing clustering on array of shape ({}, {})".format(
         *clust_data.shape), file=prfl)
 
     # Number of clusters: min is 2, max is N_cl_max
-    n_clusters = max(2, min(int(clust_data.shape[0] / N_membs), N_cl_max))
+    n_clusters = max(2, int(clust_data.shape[0] / N_membs))
+    if n_clusters > N_cl_max:
+        print("  Too many clusters. Capping at {}".format(N_cl_max), file=prfl)
+        n_clusters = N_cl_max
 
     if clust_method == 'Voronoi':
-        labels = clustAlgor.voronoi(clust_data, N_membs)
+        labels = clustAlgor.voronoi(clust_data, N_membs, n_clusters, N_st_max)
 
     # Obtain all the clusters in the input data
     elif clust_method == 'kNNdens':
         labels = clustAlgor.kNNdens(
-            clust_data, cl_method_pars, N_membs, n_clusters)
+            clust_data, cl_method_pars, N_membs, n_clusters, N_st_max)
 
     elif clust_method == 'rkmeans':
         labels = clustAlgor.RKmeans(clust_data, n_clusters)
@@ -37,25 +46,23 @@ def main(
     else:
         labels = clustAlgor.pycl(clust_data, n_clusters)
 
-    # Separate the labels that point to each cluster found
-    clusts_msk = [(labels == _) for _ in range(labels.min(), labels.max() + 1)]
-
     # import matplotlib.pyplot as plt
     # for cl in clusts_msk:
     #     plt.scatter(*clust_data[cl].T[:2], alpha=.25)
 
-    print("  Identified {} clusters".format(len(clusts_msk)), file=prfl)
+    N_clusts = len(set(labels))
+    print("  Identified {} clusters".format(N_clusts), file=prfl)
 
     # Reject "field" clusters and return their stored masks
-    C_masks, RK_vals = rjctField(
-        clust_xy, clusts_msk, clRjctMethod, KDE_vals, Kest, C_thresh,
+    C_masks, N_survived, KDE_vals = rjctField(
+        clust_xy, labels, clRjctMethod, KDE_vals, Kest, C_thresh,
         prfl)
 
-    return C_masks, RK_vals, len(clusts_msk)
+    return N_clusts, C_masks, N_survived, KDE_vals
 
 
 def rjctField(
-    clust_xy, clusts_msk, clRjctMethod, KDE_vals, Kest, C_thresh, prfl,
+    clust_xy, labels, clRjctMethod, KDE_vals, Kest, C_thresh, prfl,
         minStars=5):
     """
     Iterate over all the clusters defined by the clustering algorithm, storing
@@ -66,14 +73,17 @@ def rjctField(
                 composed of field stars and discarded.
     minStars: int
         If the cluster contains less than this many stars, skip the test and
-        mark its stars as field stars
+        mark its stars as field stars.
     """
 
-    C_masks = []
+    C_masks, N_survived = [], 0
     # For each cluster found by the clustering method, check if it is composed
     # of field stars or actual cluster members, using their (x, y)
     # distribution, and Ripley's K test.
-    for i, cl_msk in enumerate(clusts_msk):
+    # for i, cl_msk in enumerate(clusts_msk):
+    for i in range(labels.min(), labels.max() + 1):
+        # Separate stars assigned to this label
+        cl_msk = labels == i
 
         if cl_msk.sum() < minStars:
             C_s = -np.inf
@@ -94,9 +104,11 @@ def rjctField(
             # Store mask that points to stars that should be *kept*
             print("   Cluster {} survived (C_s={:.2f}), N={}".format(
                 i, C_s, cl_msk.sum()), file=prfl)
-            C_masks.append(cl_msk)
+            # Combine all the masks using a logical OR
+            C_masks = np.logical_or.reduce([C_masks, cl_msk])
+            N_survived += 1
 
-    return C_masks, KDE_vals
+    return C_masks, N_survived, KDE_vals
 
 
 def rejctVal(xy, clRjctMethod, KDE_vals, Kest):

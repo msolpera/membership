37dd5fe6e89af7cf87211a64d1e6c77b9f7fe4ea
Gabriel <gabrielperren@gmail.com>
2020-08-21 18:35:02 -0300

    minor

--- UPMASKFile_Call.py
+++ UPMASKFile_Call.py
@@ -5,4 +5,5 @@ import subprocess
 
 # Call the 'call_UPMASKFile.R' script which contains the entire UPMASK
 # package.
-subprocess.call(os.getcwd() + "/UPMASKFile.R")
+subprocess.call(os.getcwd() + "/UPMASKfile.R")
+# subprocess.call(os.getcwd() + "/UPMASKfile_modif.R")

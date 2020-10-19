fbcb44de66e2eaca57f2759279c133a2011c276d
Gabriel <gabrielperren@gmail.com>
2020-10-19 17:49:59 -0300

    test plot of PPV vs TPR

---
+++ aux_funcs/PPV_TPR.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from metrics_plot import readTables
+
+
+def main():
+    """
+    Plot the PPV vs the TPR (90%) emulating a Precision-Recall plot[1],
+    although it is *not* a PR plot as no threshold is being veried.
+
+    [1]: https://scikit-learn.org/stable/auto_examples/model_selection/
+    plot_precision_recall.html
+    """
+    configs = ("Agglo", "kNNde", "Voron", 'MiniB', "KMean", "Gauss")
+    Hval = 'auto'
+    N_UPMASK = "25"
+    flag600 = True
+
+    # Folder where the files are located
+    fold = "../TEST_SYNTH_CLUSTS/test_results/"
+
+    for i, m in enumerate(configs):
+        print(" ", m)
+        pyUP_PHOT, pyUP_PM, UP_PHOT, UP_PM = readTables(
+            fold, N_UPMASK, Hval, m, flag600)
+
+        plt.subplot(int("23" + str(i + 1)))
+        plt.title(m)
+        plt.scatter(
+            pyUP_PM['TPR_9'], pyUP_PM['PPV_9'], marker="o", c=pyUP_PM['CI'],
+            label='PM', alpha=.5)
+        plt.scatter(
+            pyUP_PHOT['TPR_9'], pyUP_PHOT['PPV_9'], marker="s",
+            c=pyUP_PHOT['CI'], label='PHOT', alpha=.5)
+        if i == 0:
+            plt.legend()
+            plt.colorbar()
+        plt.xlabel("Recall (TPR_9)")
+        plt.ylabel("Precision (PPV_9)")
+        plt.xlim(0., 1.05)
+        plt.ylim(0., 1.05)
+    plt.show()
+
+    # Now UPMASK results
+    plt.title("UPMASK")
+    plt.scatter(
+        UP_PM['TPR_9'], UP_PM['PPV_9'], marker="o", c=UP_PM['CI'],
+        label='PM', alpha=.5)
+    plt.scatter(
+        UP_PHOT['TPR_9'], UP_PHOT['PPV_9'], marker="s", c=UP_PHOT['CI'],
+        label='PHOT', alpha=.5)
+    plt.legend()
+    plt.colorbar()
+    plt.xlabel("Recall (TPR_9)")
+    plt.ylabel("Precision (PPV_9)")
+    plt.xlim(0., 1.05)
+    plt.ylim(0., 1.05)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
